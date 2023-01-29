a8a70ecf17c1d53df863c837e1ac9ef82bc0adae
root <lizongzai@gmail.com>
2023-01-29 14:40:47 +0800

    v6

--- yeb-server/src/main/java/com/xxxx/server/config/redis/RedisConfig.java
+++ yeb-server/src/main/java/com/xxxx/server/config/redis/RedisConfig.java
@@ -3,22 +3,24 @@ package com.xxxx.server.config.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Redis配置
+ * @author lizongzai
+ * @since 1.0.0
  */
 
 @Configuration
 public class RedisConfig {
 
   @Bean
-  public RedisTemplate<String,Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 
-    RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
     //String类型key设置序列器
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     //String类型value设置序列其
