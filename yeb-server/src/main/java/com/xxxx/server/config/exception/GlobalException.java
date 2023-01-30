68abc24975ef58bd5a8d5fde1cf5d3f7cd0eabd4
root <lizongzai@gmail.com>
2023-01-30 16:18:18 +0800

    v7

---
+++ yeb-server/src/main/java/com/xxxx/server/config/exception/GlobalException.java
@@ -0,0 +1,25 @@
+package com.xxxx.server.config.exception;
+
+import com.xxxx.server.pojo.RespBean;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理
+ *
+ * @author lizongzai
+ * @since 1.0.0
+ */
+@RestControllerAdvice
+public class GlobalException {
+
+  @ExceptionHandler({SQLException.class})
+  public RespBean mySqlException(SQLException sqlException) {
+    if (sqlException instanceof SQLIntegrityConstraintViolationException) {
+      return RespBean.error("该数据有关联数据约束,操作失败!");
+    }
+    return RespBean.error("数据库异常,操作失败!");
+  }
+}
