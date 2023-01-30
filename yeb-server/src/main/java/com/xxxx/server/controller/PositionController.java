17f6256251ac7426e5f75e7612fcd5e7ff3f08a3
root <lizongzai@gmail.com>
2023-01-30 15:40:52 +0800

    v7

--- yeb-server/src/main/java/com/xxxx/server/controller/PositionController.java
+++ yeb-server/src/main/java/com/xxxx/server/controller/PositionController.java
@@ -1,19 +1,80 @@
 package com.xxxx.server.controller;
 
 
+import com.xxxx.server.mapper.PositionMapper;
+import com.xxxx.server.pojo.Position;
+import com.xxxx.server.pojo.RespBean;
+import com.xxxx.server.service.IPositionService;
+import io.swagger.annotations.ApiOperation;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author lizongzai
  * @since 2023-01-19
  */
 @RestController
-@RequestMapping("/position")
+@RequestMapping("/system/basic/pos")
 public class PositionController {
+
+  @Autowired
+  private IPositionService positionService;
+
+  @ApiOperation(value = "获取所有职位信息")
+  @GetMapping("/")
+  public List<Position> getAllPositions() {
+    return positionService.list();
+  }
+
+  @ApiOperation(value = "添加职位信息")
+  @PostMapping("/")
+  public RespBean addPosition(@RequestBody Position position) {
+    position.setCreateDate(LocalDateTime.now());
+    if (positionService.save(position)) {
+      return RespBean.success("添加成功");
+    }
+    return RespBean.error("添加失败!");
+  }
+
+  @ApiOperation(value = "更新职位信息")
+  @PutMapping("/")
+  public RespBean updatePosition(@RequestBody Position position) {
+    if (positionService.updateById(position)) {
+      return RespBean.success("更新成功!");
+    }
+    return RespBean.error("更新失败!");
+  }
+
+  @ApiOperation(value = "删除职位信息")
+  @DeleteMapping("/{id}")
+  public RespBean removePositionById(Integer id) {
+    if (positionService.removeById(id)) {
+      return RespBean.success("删除成功");
+    }
+    return RespBean.error("删除成功!");
+  }
+
+  @ApiOperation(value = "批量删除职位")
+  @DeleteMapping("/")
+  public RespBean removePositionByIds(Integer[] ids) {
+    if (positionService.removeByIds(Arrays.asList(ids))) { //需要转数组
+      return RespBean.success("删除成功!");
+    }
+    return RespBean.error("删除失败!");
+  }
 
 }
