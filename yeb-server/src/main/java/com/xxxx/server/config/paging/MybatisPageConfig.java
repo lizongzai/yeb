050613ca6377890c0f321e634d9dd7021ccf684c
root <lizongzai@gmail.com>
2023-02-07 11:52:03 +0800

    V28 MybatisPage分页插件配置

--- yeb-server/src/main/java/com/xxxx/server/config/paging/MybatisPageConfig.java
+++ yeb-server/src/main/java/com/xxxx/server/config/paging/MybatisPageConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MybatisPageConfig {
-
   @Bean
   public PaginationInnerInterceptor paginationInnerInterceptor() {
     return new PaginationInnerInterceptor();
